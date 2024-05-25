f28945b25e426cb8607ad3d159433301c44e4c4e
root <root@LAPTOP-FA299JUM.localdomain>
2024-05-25 22:18:33 +0800

    Create Layout for Tutor Signup

--- ryderz/urls.py
+++ ryderz/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("", views.index, name="index")
+  path("", views.index, name="index"),
+  path("tutorsignup/", views.tutor_signup, name="tutor_signup")
 ]
--- ryderz/views.py
+++ ryderz/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
+from django import forms
 
-# Create your views here.
+class TeacherSignUpForm(forms.Form):
+  CHOICES = [
+    ('whatsapp', 'Whatsapp'),
+    ('telegram', 'Telegram'),
+    ('email', 'Email'),
+    ('messages', 'Messages'),
+  ]
+
+  GENDERS = [
+    ('male', 'Male'),
+    ('female', 'Female'),
+  ]
+
+  username = forms.CharField(max_length=32, label="Username")
+  password = forms.CharField(max_length=64, label="Password")
+  name = forms.CharField(max_length=256, label="Full Name")
+  email = forms.EmailField(max_length=256, label="Email") 
+  sex = forms.ChoiceField(choices=GENDERS, label='Select Your Gender')
+  birthday = forms.DateField(label="Date Of Birth")
+  phone_number = forms.IntegerField(min_value=8, max_value=8, label="Phone Number")
+  contact_method = forms.ChoiceField(choices=CHOICES, label='Select Preferred Contact Method')
+  university = forms.CharField(max_length=256, label='Univeristy')
+  availability = forms.CharField(max_length=256, label='Available Times')
+  resume = forms.CharField(max_length=1024, label='Resume')
+  about_me = forms.CharField(max_length=1024, label='About Me')
+  picture = forms.ImageField()
+
 def index(request):
   return render(request, "ryderz/index.html")
+
+def tutor_signup(request):
+  form = TeacherSignUpForm()
+  return render(request, "ryderz/tutor_signup.html", {
+    'form': form
+  })
+
